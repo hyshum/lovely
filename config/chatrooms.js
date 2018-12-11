49033edf7525d254c09fcf82374bb339da78e606
Ronnie <ronnieshum1996@gmail.com>
2018-12-11 04:33:33 -0500

    First

---
+++ config/chatrooms.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    name: 'EC441: Computer Networking',
+    image: 'chatrooms/network2.jpg'
+  },
+  {
+    name: 'CS411: Software Engineering',
+    image: 'chatrooms/agile.jpg'
+  },
+  {
+    name: 'EC504: Advanced Data Structure',
+    image: 'chatrooms/data.png'
+  }
+]
+
---
+++ config/users.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    name: 'Cong',
+    lastName: 'Han',
+    statusText: 'What\'s up!',
+    image: 'users/cong.png'
+  },
+  {
+    name: 'Zexing',
+    lastName: 'Gao',
+    statusText: 'Skr Skr~',
+    image: 'users/xing.png'
+  },
+  {
+    name: 'Ronnie',
+    lastName: 'Shum',
+    statusText: 'I like bubble tea.',
+    image: 'users/ronnie.png'
+  }
+]
+
